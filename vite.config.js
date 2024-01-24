3e437e640237a07a6066b4d362e44bb2866712e3
goutam2690 <124591682+goutam2690@users.noreply.github.com>
2024-01-24 13:17:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/',
+  server : {
+    host: true,
+    strictPort: true,
+    port: 2690
+ }
 })
